a652267e7070ab5f16bfd2bda92e13c94c990d13
Moti Korets <moti.korets@riskmethods.net>
2019-06-22 12:53:29 +0200

    add options template

---
+++ options.js
@@ -0,0 +1,12 @@
+const defaultTranslations = [
+  {
+    name: "google",
+    label: "Google Tranlsate",
+    url: "https://translate.google.com/?hl=en#auto/en/"
+  },
+  {
+    name: "dict",
+    label: "dict.cc",
+    url: "http://pocket.dict.cc/?s="
+  }
+];
