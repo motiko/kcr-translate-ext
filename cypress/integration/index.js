3418405ac6321a80ff17c0664b73210e3522bc5c
Moti Korets <moti.korets@riskmethods.net>
2019-12-03 20:49:09 +0100

    add cypress
    switch to yarn

---
+++ cypress/integration/index.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+context('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('https://read.amazon.com/')
+    cy.get('.navbar-nav').contains('Commands').click()
+    cy.get('.dropdown-menu').contains('Navigation').click()
+  })
+
+  it('cy.visit() - visit a remote url', () => {
+
+  })
+})
