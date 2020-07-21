287c2b748e3d9a477ab17c76aa45a67539341ff2
moti <moti.kor@gmail.com>
2020-07-21 17:29:00 +0200

    fix autoread

--- chrome/autoplay.js
+++ chrome/autoplay.js
@@ -11,17 +11,39 @@ chrome.runtime.sendMessage({ command: "ACTIVATE_PAGE_ACTION" });
 chrome.runtime.sendMessage({ command: "GET_SETTINGS" }, function (response) {
   if (response) {
     if (response && response.autoread) {
-      inject(function () {
-        const srcPlay = document.querySelector(
-          "div.source-footer > div.src-tts"
-        );
-        setTimeout(() => {
-          srcPlay.dispatchEvent(new MouseEvent("mousedown"));
-          srcPlay.dispatchEvent(new MouseEvent("mouseup"));
-        }, 100);
-      });
+      inject(main);
     }
   } else {
     console.error(chrome.runtime.lastError.message);
   }
 });
+
+function main() {
+  const srcPlay = document.querySelector("div.source-footer > div.src-tts");
+  const container = document.querySelector(".input-button-container");
+  const sourceText = document.getElementById("source");
+  let autoreadInterval;
+  let lastAutoreadText;
+  let autoreadEnabled = true;
+  window.onhashchange = function () {
+    autoRead();
+  };
+
+  const autoRead = () => {
+    if (autoreadEnabled) {
+      srcPlay.dispatchEvent(new MouseEvent("mousedown"));
+      srcPlay.dispatchEvent(new MouseEvent("mouseup"));
+    }
+  };
+
+  let cb = document.createElement("span");
+  cb.innerHTML = `<input type="checkbox" id="auto_read" checked="checked" />
+                        <label for="auto_read">Autoread</label>`;
+  container.appendChild(cb);
+  document
+    .querySelector("#auto_read")
+    .addEventListener("click", function ({ target }) {
+      autoreadEnabled = target.checked;
+    });
+  setTimeout(autoRead, 500);
+}
