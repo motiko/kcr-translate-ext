36087df348fe44a88eb88a50e9eaffc29c678d0d
p-mazhnik <pavel.mazhnik@gmail.com>
2022-01-21 17:58:01 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin');
+
+const inputDir = path.join(__dirname, 'chrome');
+const outputDir = path.join(__dirname, 'dist');
+
+module.exports = {
+  entry: {
+    autoplay: path.join(inputDir, 'autoplay.js'),
+    index: path.join(inputDir, 'index.js'),
+    ocr: path.join(inputDir, 'ocr.js'),
+    options: path.join(inputDir, 'options.js'),
+    background: path.join(inputDir, 'background.js'),
+  },
+  output: {
+    path: outputDir,
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]',
+        },
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new WebpackExtensionManifestPlugin({
+      config: {
+        base: path.join(inputDir, 'manifest.json'),
+      },
+      pkgJsonProps: [
+        'version',
+        'description',
+      ]
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(inputDir, 'img'),
+          to: path.join(outputDir, "img"),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(inputDir, "ocr.html"),
+      filename: "ocr.html",
+      chunks: ["ocr"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(inputDir, "options.html"),
+      filename: "options.html",
+      chunks: ["options"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(inputDir, "background.html"),
+      filename: "background.html",
+      chunks: ["background"]
+    }),
+  ],
+};
