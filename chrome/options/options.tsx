8d62c0bf94c3930c43d2699787009570d8795576
Moti Korets <875618+motiko@users.noreply.github.com>
2022-01-23 11:41:20 +0100

    prettify options

--- chrome/options/options.tsx
+++ chrome/options/options.tsx
@@ -1,33 +1,41 @@
-import React, { ChangeEventHandler, useEffect, useState } from 'react'
-import { Engines, ITranslateEngine, tesseractLangs} from '../const'
-import { DictCCEngineOptions } from './dict/options'
-import { GoogleTranslateEngineOptions, GoogleTranslateExtEngineOptions } from './google/options'
-import { Storage } from '../services/storage'
-import { showMessage } from './utils'
+import React, { ChangeEventHandler, useEffect, useState } from "react";
+import { Engines, ITranslateEngine, tesseractLangs } from "../const";
+import { DictCCEngineOptions } from "./dict/options";
+import {
+  GoogleTranslateEngineOptions,
+  GoogleTranslateExtEngineOptions,
+} from "./google/options";
+import { Storage } from "../services/storage";
+import { showMessage } from "./utils";
 
 interface IEngineSelectProps {
-  translateEngines: ITranslateEngine[],
-  onChangeEngine: (value: Engines) => void,
-  selectedEngineName: string,
+  translateEngines: ITranslateEngine[];
+  onChangeEngine: (value: Engines) => void;
+  selectedEngineName: string;
 }
 
-const EnginesSelect = ({ translateEngines, onChangeEngine, selectedEngineName }: IEngineSelectProps) => {
+const EnginesSelect = ({
+  translateEngines,
+  onChangeEngine,
+  selectedEngineName,
+}: IEngineSelectProps) => {
   const onSelect: ChangeEventHandler<HTMLSelectElement> = (event) => {
     onChangeEngine(event.target.value as Engines);
-  }
+  };
   return (
-    <select id="translate_engine" onChange={onSelect} value={selectedEngineName}>
-      {
-        translateEngines.map(
-          (engine) =>
-            <option key={engine.name} value={engine.name}>
-              {engine.label}
-            </option>
-        )
-      }
+    <select
+      id="translate_engine"
+      onChange={onSelect}
+      value={selectedEngineName}
+    >
+      {translateEngines.map((engine) => (
+        <option key={engine.name} value={engine.name}>
+          {engine.label}
+        </option>
+      ))}
     </select>
-  )
-}
+  );
+};
 
 const Options = () => {
   const storage = new Storage();
@@ -38,11 +46,11 @@ const Options = () => {
     onEngineUpdate({
       ...engines.find((t) => t.name === engineName)!,
       selected: true,
-    })
-  }
+    });
+  };
   const onEngineUpdate = (newEngineData: ITranslateEngine) => {
     const { selected } = newEngineData;
-    const newEngines = engines.map(e => {
+    const newEngines = engines.map((e) => {
       if (newEngineData.name === e.name) {
         return newEngineData;
       }
@@ -50,27 +58,29 @@ const Options = () => {
         return {
           ...e,
           selected: false,
-        }
+        };
       }
       return e;
-    })
+    });
     setEngines(newEngines);
-  }
-  const onOcrLanguageSelect: ChangeEventHandler<HTMLSelectElement> = (event) => {
+  };
+  const onOcrLanguageSelect: ChangeEventHandler<HTMLSelectElement> = (
+    event
+  ) => {
     setOcrLangs(event.target.value);
-  }
+  };
   const onSaveBtnClick = () => {
     storage.updateValues(engines, ocrLangs).then(() => {
       showMessage("saved_message");
-    })
-  }
+    });
+  };
   const onRestoreBtnClick = () => {
-    storage.restoreDefaultSettings().then(({translateEngines, ocrLangs}) => {
+    storage.restoreDefaultSettings().then(({ translateEngines, ocrLangs }) => {
       showMessage("defaults_restored_message");
       setEngines(translateEngines);
       setOcrLangs(ocrLangs);
-    })
-  }
+    });
+  };
   useEffect(() => {
     // load data from storage
     const loadFromStorage = async () => {
@@ -79,34 +89,36 @@ const Options = () => {
       const engines = await storage.getTranslateEngines();
       setEngines(engines);
       setLoaded(true);
-    }
+    };
     void loadFromStorage();
-  }, [])
+  }, []);
   if (!loadedFromStorage) {
     return <div className="section" />;
   }
-  const selectedEngine = engines.find(e => e.selected)!;
+  const selectedEngine = engines.find((e) => e.selected)!;
   const onChangeUrl: ChangeEventHandler<HTMLInputElement> = (event) => {
     onEngineUpdate({
       ...selectedEngine,
       url: event.target.value,
-    })
-  }
+    });
+  };
   return (
     <div className="section">
       <div className="container">
         <form id="shortcutsForm" name="shortcutsForm">
           <div className="row container center-content">
-            <label htmlFor="ocrLangs">
-              Translate From </label>
-            <select className="u-full-width" id="ocrLangs" value={ocrLangs} onChange={onOcrLanguageSelect}>
-              {
-                Object.entries(tesseractLangs).map(([langKey,langName]) => (
-                  <option key={langKey} value={langKey}>
-                    {langName}
-                  </option>
-                ))
-              }
+            <label htmlFor="ocrLangs">Translate From </label>
+            <select
+              className="u-full-width"
+              id="ocrLangs"
+              value={ocrLangs}
+              onChange={onOcrLanguageSelect}
+            >
+              {Object.entries(tesseractLangs).map(([langKey, langName]) => (
+                <option key={langKey} value={langKey}>
+                  {langName}
+                </option>
+              ))}
             </select>
             <EnginesSelect
               translateEngines={engines}
@@ -116,16 +128,42 @@ const Options = () => {
             {selectedEngine.name !== Engines.GOOGLE_TRANSLATE_EXT && (
               <>
                 <label htmlFor="url">Url</label>
-                <input className="u-full-width" type="text" id="url" value={selectedEngine.url} onChange={onChangeUrl} />
+                <input
+                  className="u-full-width"
+                  type="text"
+                  id="url"
+                  value={selectedEngine.url}
+                  onChange={onChangeUrl}
+                />
               </>
             )}
-            <GoogleTranslateEngineOptions selectedEngine={selectedEngine} onEngineUpdate={onEngineUpdate} />
-            <DictCCEngineOptions selectedEngine={selectedEngine} onEngineUpdate={onEngineUpdate} />
-            <GoogleTranslateExtEngineOptions selectedEngine={selectedEngine} onEngineUpdate={onEngineUpdate} />
+            <GoogleTranslateEngineOptions
+              selectedEngine={selectedEngine}
+              onEngineUpdate={onEngineUpdate}
+            />
+            <DictCCEngineOptions
+              selectedEngine={selectedEngine}
+              onEngineUpdate={onEngineUpdate}
+            />
+            <GoogleTranslateExtEngineOptions
+              selectedEngine={selectedEngine}
+              onEngineUpdate={onEngineUpdate}
+            />
             <div className="row container center-content">
-              <a id="restore_defaults_btn" className="button-primary button" onClick={onRestoreBtnClick}>
+              <a
+                id="restore_defaults_btn"
+                className="button-primary button"
+                onClick={onRestoreBtnClick}
+              >
                 Restore Defaults
-              </a> <a id="save_btn" className="button-primary button" onClick={onSaveBtnClick}>Save</a>
+              </a>
+              <a
+                id="save_btn"
+                className="button-primary button"
+                onClick={onSaveBtnClick}
+              >
+                Save
+              </a>
             </div>
             <div className="row container center-content">
               <div className="opaque message" id="saved_message">
@@ -143,6 +181,6 @@ const Options = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Options;
